6c479c5a24d7bc9549aea9fe2f7516e8e64ded5e
Malik Abdul Kawee <malik.kawee@gmail.com>
2023-02-19 19:41:18 +0500

    #removed log

--- src/components/common/appSelect/index.tsx
+++ src/components/common/appSelect/index.tsx
@@ -16,30 +16,27 @@ const AppSelectComp: React.FC<AppSelectProps> = props => {
   const {heading, containerProps, headingTextProps, children, error, ...rest} =
     props;
   return (
-    <>
-      {console.log('selecte rendered')}
-      <VStack {...containerProps}>
-        <Text fontSize={'md'} {...headingTextProps}>
-          {heading}
-        </Text>
-        <Select
-          mt={'1'}
-          mx={{
-            base: 0,
-            md: 'auto',
-          }}
-          borderColor={error ? 'red.400' : null}
-          fontSize={'md'}
-          dropdownIcon={<ChevronDownIcon mr={'3'} size={4} />}
-          _selectedItem={{
-            bg: 'primary.400',
-            tintColor: 'white',
-          }}
-          {...rest}>
-          {children}
-        </Select>
-      </VStack>
-    </>
+    <VStack {...containerProps}>
+      <Text fontSize={'md'} {...headingTextProps}>
+        {heading}
+      </Text>
+      <Select
+        mt={'1'}
+        mx={{
+          base: 0,
+          md: 'auto',
+        }}
+        borderColor={error ? 'red.400' : null}
+        fontSize={'md'}
+        dropdownIcon={<ChevronDownIcon mr={'3'} size={4} />}
+        _selectedItem={{
+          bg: 'primary.400',
+          tintColor: 'white',
+        }}
+        {...rest}>
+        {children}
+      </Select>
+    </VStack>
   );
 };
 
